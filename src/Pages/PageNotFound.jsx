56b843ee24d331e8df60f99808c7aaaf7e5e971b
SandhraShaji <sandhrashajik@gmail.com>
2024-04-07 23:13:14 +0530

    frontend for bookstore

---
+++ src/Pages/PageNotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Components/Header'
+
+function PageNotFound() {
+  return (
+    <div>
+      <Header/>
+      <div id='d4' className='text-center'>
+        <img src="https://blog.thomasnet.com/hs-fs/hubfs/shutterstock_774749455.jpg?width=900&name=shutterstock_774749455.jpg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default PageNotFound
---
+++ src/services/baseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl = 'https://backend-for-bookstore.onrender.com'
