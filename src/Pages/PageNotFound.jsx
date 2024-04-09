ca3e78571cd530b9015a14ac5b19d70547c9ce90
SandhraShaji <sandhrashajik@gmail.com>
2024-04-09 19:05:08 +0530

    update

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -3,10 +3,10 @@ import Header from '../Components/Header'
 
 function PageNotFound() {
   return (
-    <div>
+    <div style={{backgroundColor:'rgba(244,222,203,0.7)'}}>
       <Header/>
-      <div id='d4' className='text-center'>
-        <img src="https://blog.thomasnet.com/hs-fs/hubfs/shutterstock_774749455.jpg?width=900&name=shutterstock_774749455.jpg" alt="" />
+      <div id='d4' className='text-center py-5'>
+        <img style={{border:'2px solid rgb(176,113,84)'}} className='my-5' src="https://blog.thomasnet.com/hs-fs/hubfs/shutterstock_774749455.jpg?width=900&name=shutterstock_774749455.jpg" alt="" />
       </div>
     </div>
   )
